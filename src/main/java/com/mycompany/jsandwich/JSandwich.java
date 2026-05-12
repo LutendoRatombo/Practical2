a849d4d6ec1e1491d24c7727f27b4c9e00e7b285
Lutendo Ratombo <Lutendo>
2026-05-12 15:50:42 +0200

    First commit

---
+++ src/main/java/com/mycompany/jsandwich/JSandwich.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jsandwich;
+
+/**
+ *
+ * @author luten
+ */
+public class JSandwich {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
